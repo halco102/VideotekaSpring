c28ad63f1d1c9863d50e086783afbe30a2d9b708
halco102 <ado.halilovic@outlook.com>
2020-09-17 17:34:34 +0200

    commit

--- Videoteka/src/main/java/com/DiplomskiRad/Videoteka/controller/MovieController.java
+++ Videoteka/src/main/java/com/DiplomskiRad/Videoteka/controller/MovieController.java
@@ -5,6 +5,7 @@ import com.DiplomskiRad.Videoteka.domain.Movie;
 import com.DiplomskiRad.Videoteka.repositories.MovieRepository;
 import com.DiplomskiRad.Videoteka.service.MovieService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +15,15 @@ import java.util.List;
 @RequestMapping(MovieController.BASE_URL)
 public class MovieController {
 
-    public static final String BASE_URL="/api/v1/movies";
+    public static final String BASE_URL="/api/v1/videoteka";
     private final MovieService movieService;
 
     public MovieController(MovieService movieService) {
         this.movieService = movieService;
     }
 
-    @GetMapping()
+    /*
+    @GetMapping("/movies")
     List<Movie> findAllMovies(){
         return  movieService.findAllMovies();
     }
@@ -29,6 +31,13 @@ public class MovieController {
     @GetMapping("/{id}")
     Movie findMoviebyId(@PathVariable  Long id){
         return this.movieService.findMoviebyId(id);
+    }
+    */
+
+    @RequestMapping("/movies")
+    public String getMovies(Model model){
+        model.addAttribute("movies",movieService.findAllMovies());
+        return "videoteka/index.html";
     }
 
 }
