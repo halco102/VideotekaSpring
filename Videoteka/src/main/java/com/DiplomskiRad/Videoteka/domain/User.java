c8e877f243b96b729a1dfc789f7031c690673020
halco102 <ado.halilovic@outlook.com>
2020-10-10 14:45:19 +0200

    commit

--- Videoteka/src/main/java/com/DiplomskiRad/Videoteka/domain/User.java
+++ Videoteka/src/main/java/com/DiplomskiRad/Videoteka/domain/User.java
@@ -11,26 +11,22 @@ import javax.validation.constraints.Size;
 public class User {
 
     @Id
+    @Column(name="id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "first_name")
-    @Size(min=2,max=40,message = "First name has to be 2 to 40 characters long")
     private String firstName;
 
     @Column(name = "last_name")
-    @Size(min=2,max=40,message = "Last name has to be 2 to 40 characters long")
     private String lastName;
 
-    @Size(min=2,max=40,message = "Username has to be 2 to 40 characters long")
     @Column(name = "username")
     private String userName;
 
-    @Min(value = 2,message = "Min lenght of password is 2")
     @Column(name = "password")
     private String password;
 
-    @Size(min=2,max=40,message = "email has to be 2 to 40 characters long")
     @Column(name = "email", unique = true)
     private String eMail;
 
